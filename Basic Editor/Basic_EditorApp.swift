fb1141c1d8680c61f5f0d126d38a580efd9ec08f
George Babichev <gbabichev@xantrion.com>
2026-04-05 10:48:05 -0400

    Initial Commit

---
+++ Basic Editor/Basic_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_EditorApp.swift
+//  Basic Editor
+//
+//  Created by George Babichev on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Basic_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic Editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Basic Editor
+//
+//  Created by George Babichev on 4/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
